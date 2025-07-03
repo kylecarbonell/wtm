80f609b507b75646ef97feab97780045aa49bb86
Kyle Carbonell <Kylecarbonell13@gmail.com>
2025-07-02 21:11:28 -0700

    [Fix] Formatting: Fixed up formatting with prettier

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 export default function Header() {
     return (
         <>
-            <View className='flex h-12 items-center justify-center flex-row'>
-                <Text className='text-3xl font-semibold'>WTM</Text>
+            <View className="flex h-12 flex-row items-center justify-center">
+                <Text className="text-3xl font-semibold">WTM</Text>
             </View>
         </>
     );

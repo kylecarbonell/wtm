80c97e7b410e10212c55cd6ea96e5c4c21168c31
Kyle Carbonell <Kylecarbonell13@gmail.com>
2025-07-02 20:59:44 -0700

    [Chore] Formatting : Ran prettier formatter to format all files

--- client/components/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { SafeAreaView } from 'react-native';
-
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className="bg-red-600">{children}</SafeAreaView>;
-};

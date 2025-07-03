0a34746efb0b40d034a1e61f15536e4774cb8b41
Kyle Carbonell <Kylecarbonell13@gmail.com>
2025-07-02 22:25:32 -0700

    [Feature] GORM: Set up database connection with GORM

---
+++ server/db/main.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	err := godotenv.Load()
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+	
+	log.Println(dsn)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database:", err)
+	}
+
+	DB = db
+
+	// Migrations to add in tables
+	fmt.Println("✅ Connected to database")
+}

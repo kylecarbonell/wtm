2e5aba923a90f9e4fab127754e358dc9fce75480
Kyle Carbonell <Kylecarbonell13@gmail.com>
2025-07-02 21:00:05 -0700

    [Feature] Header: Created basic header design

---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Header() {
+    return (
+        <>
+            <View className='flex h-12 items-center justify-center'>
+                <Text className='text-3xl'>WTM</Text>
+            </View>
+        </>
+    );
+}

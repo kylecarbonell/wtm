6b68f519c768914a29d9fb81dc3f6f50eeae8d41
Kyle Carbonell <Kylecarbonell13@gmail.com>
2025-07-04 07:42:05 -0400

    [Feature] Post Component: Created static barebones component for each post

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 
 import '../global.css';
 import Header from 'components/Header';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
+import Post from 'components/Post';
 
 export default function App() {
     return (
         <>
             <SafeAreaView>
                 <Header></Header>
+                <ScrollView className="mt-4 h-screenpb-32">
+                    {Array(5)
+                        .fill(0)
+                        .map((val, i) => (
+                            <Post key={i}></Post>
+                        ))}
+                </ScrollView>
                 <StatusBar style="auto" />
             </SafeAreaView>
         </>
